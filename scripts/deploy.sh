c2c9b64a9e3a5bda822cb286f66316142ba8bbaf
Nalini <Nalini.s.meg@gmail.com>
2026-07-04 19:08:40 +0530

    modifed jenkins file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -e
-PROD_REPO_NAME="naliniselv/react-app-prod:latest"
 DEV_REPO_NAME="naliniselv/react-app-dev:latest"
-if [ "$BRANCH_NAME" == "dev" ]; then
-    IMAGE_NAME=$DEV_REPO_NAME
-else
-    IMAGE_NAME=$PROD_REPO_NAME
-fi
 CONTAINER_NAME="react-app-container"
-echo "Deploying $IMAGE_NAME to $CONTAINER_NAME"
-cd "${dirname "$0"}/../docker" # change to the docker directory one level up from the script's location
-docker pull $IMAGE_NAME # pull the latest image from the repository
+echo "Deploying $DEV_REPO_NAME to $CONTAINER_NAME"
+cd "$(dirname "$0")/../docker" # change to the docker directory one level up from the script's location
+docker pull $DEV_REPO_NAME # pull the latest image from the repository
 docker stop $CONTAINER_NAME || true # stop the existing container if it exists
 docker rm $CONTAINER_NAME || true # remove the existing container if it exists
-docker run -d --name $CONTAINER_NAME -p 80:80 $IMAGE_NAME # run the new container with the pulled image
+docker run -d --name $CONTAINER_NAME -p 80:80 $DEV_REPO_NAME # run the new container with the pulled image
