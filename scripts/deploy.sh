08ba97232e5c65b68c997f2c3774a56d0d5c57e8
Nalini <Nalini.s.meg@gmail.com>
2026-07-04 21:34:55 +0530

    modifed jenkins file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-DEV_REPO_NAME="naliniselv/react-app-dev:latest"
+PROD_REPO_NAME="naliniselv/react-app-prod:latest"
 CONTAINER_NAME="react-app-container"
-echo "Deploying $DEV_REPO_NAME to $CONTAINER_NAME"
+echo "Deploying $PROD_REPO_NAME to $CONTAINER_NAME"
 cd "$(dirname "$0")/../docker" # change to the docker directory one level up from the script's location
-docker pull $DEV_REPO_NAME # pull the latest image from the repository
+docker pull $PROD_REPO_NAME # pull the latest image from the repository
 docker stop $CONTAINER_NAME || true # stop the existing container if it exists
 docker rm $CONTAINER_NAME || true # remove the existing container if it exists
-docker run -d --name $CONTAINER_NAME -p 80:80 $DEV_REPO_NAME # run the new container with the pulled image
+docker run -d --name $CONTAINER_NAME -p 80:80 $PROD_REPO_NAME # run the new container with the pulled image
